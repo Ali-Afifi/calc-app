6bdf30fa172d62fed87fdd6d9293f5d63ae153e0
Ali-Afifi <ali.m.afify2001@gmail.com>
2021-04-13 13:32:34 +0200

    fixed HTML and CSS and added JS

---
+++ main.js
@@ -0,0 +1,13 @@
+const numBtns = document.getElementsByClassName("num");
+const equal = document.getElementById("equal");
+const add = document.getElementById("addition");
+const sub = document.getElementById("subtraction");
+const dot = document.getElementById("dot");
+const multiplication = document.getElementById("multiplication");
+const division = document.getElementById("division");
+const clear = document.getElementById("clear");
+const del = document.getElementById("delete");
+const leftP = document.getElementById("leftP");
+const rightP = document.getElementById("rightP");
+const display = document.getElementById("screen-text");
+
