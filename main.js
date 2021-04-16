eb851117eb3f8cc2d893efa8689befa4bda9d93a
Ali-Afifi <ali.m.afify2001@gmail.com>
2021-04-16 17:43:17 +0200

    finished project

--- main.js
+++ main.js
@@ -1,13 +1,29 @@
-const numBtns = document.getElementsByClassName("num");
+const nums = document.getElementsByClassName("num");
+const syms = document.getElementsByClassName("sym");
 const equal = document.getElementById("equal");
-const add = document.getElementById("addition");
-const sub = document.getElementById("subtraction");
-const dot = document.getElementById("dot");
-const multiplication = document.getElementById("multiplication");
-const division = document.getElementById("division");
 const clear = document.getElementById("clear");
 const del = document.getElementById("delete");
-const leftP = document.getElementById("leftP");
-const rightP = document.getElementById("rightP");
 const display = document.getElementById("screen-text");
 
+
+function displayContent(e) {
+    if (display.textContent.length <= 8) {
+        display.textContent += e.target.textContent;
+    }
+}
+
+clear.addEventListener("click", () => display.textContent = "");
+
+equal.addEventListener("click", () => {
+    display.textContent = Math.round(eval(display.textContent.replace("X", "*"))*10000000)/10000000;
+});
+
+del.addEventListener("click", () => display.textContent = display.textContent.slice(0, display.textContent.length - 1));
+
+for (let i = 0; i < nums.length; i++) {
+    nums[i].addEventListener("click", displayContent);
+}
+
+for (let i = 0; i < syms.length; i++) {
+    syms[i].addEventListener("click", displayContent);
+}
